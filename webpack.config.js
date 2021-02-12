05d05d2046a53a770c0a9a88f882da9c4262c92a
pailhead <bodu3d@gmail.com>
2021-02-11 23:57:27 -0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = {
   entry: './src/index.ts',
   mode: 'production',
+  devtool: 'source-map',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
